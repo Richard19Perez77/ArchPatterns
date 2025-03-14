b96fe4d745a52801326569b0e516ad85b9fc1d19
richa <richard19.perez77@yahoo.com>
2025-03-14 22:09:54 +0700

    add hilt to main activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,9 +49,10 @@ dependencies {
     kapt("com.google.dagger:hilt-compiler:2.54")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0") // JSON conversion
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation(libs.androidx.core.ktx)
